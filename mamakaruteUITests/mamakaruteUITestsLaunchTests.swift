9512cc52934396434b875824d7af14c09c48dd78
powertype2020 <105772633+powertype2020@users.noreply.github.com>
2022-07-15 12:17:07 +0900

    Initial Commit

---
+++ mamakaruteUITests/mamakaruteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mamakaruteUITestsLaunchTests.swift
+//  mamakaruteUITests
+//
+//  Created by 武久　直幹 on 2022/07/15.
+//
+
+import XCTest
+
+class mamakaruteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
